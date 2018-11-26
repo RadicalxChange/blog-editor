2dbab1641384d45114bd2b6991ba1e294a6278d0
Matt Masurka <m.masurka@gmail.com>
2018-11-25 23:48:52 -0800

    Fixes regex error

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,17 @@
 export function formatForHoverboard(html) {
-  let hoverHTML = "";
-  const images = html.match(/(?<=<img\ssrc=")(.*)(?="\s)/g);
+  let hoverHTML = html;
+  let images = html.match(/<img.+?>/g);
 
   if (images) {
+    images = images.map(image => image.match(/http.+?[^"]*/)[0]);
+    console.log(images);
     images.forEach(url => {
-      hoverHTML = html.replace(
+      hoverHTML = hoverHTML.replace(
         /<img.+?>/,
         `<plastic-image srcset="${url}" lazy-load preload fade></plastic-image>`
       );
     });
   }
 
-  if (hoverHTML) return hoverHTML;
-  return html;
+  return hoverHTML;
 }
