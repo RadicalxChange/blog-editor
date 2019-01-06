8c48c03ec6a14098a85a1b203b3f5cdb73172cad
Matt Masurka <m.masurka@gmail.com>
2019-01-05 17:03:57 -0800

    Fixes date saving & parsing problem that was causing an error in Safari

--- src/App.js
+++ src/App.js
@@ -297,8 +297,11 @@ class App extends React.Component {
   getTodayDate = () => {
     let date = new Date();
     let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");
+    let day = date
+      .getDate()
+      .toString()
+      .padStart(2, "0");
     return `${year}-${month}-${day}`;
   };
 
